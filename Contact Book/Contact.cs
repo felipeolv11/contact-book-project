4e0f84631d7e33860414fd2e51019076b91c4307
felipeolv11 <felipeeh1524@gmail.com>
2025-07-24 01:42:34 -0300

    Add project files.

---
+++ Contact Book/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ContactBook
+{
+    class Contact
+    {
+        public string name { get; set; }
+        public int phone { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+
+        public Contact(string name, int phone, string email, string address)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+            this.address = address;
+        }
+    }
+}
